b7b1cfa1d0a14fa58ca42fe2377f8378c7a16f2c
kebere787 <kebere787@gmail.com>
2022-09-13 21:04:15 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  *main - print the string in the put function
  *
- * description: using the main function
+ * Description: using the main function
  * this program print "programming is like building a multilingual puzzle
  * Return: 0
  */
