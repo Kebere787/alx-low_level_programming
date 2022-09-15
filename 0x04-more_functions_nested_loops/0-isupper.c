633f9937ebaa725809df04cfe6d03540c0788808
kebere787 <kebere787@gmail.com>
2022-09-15 17:21:43 +0300

    0th task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+  *
+*[B
+*
+*[A[A  *[A _isupper - check if a letter is upper
+[C[C[C 0x: The number to be checked
+  *
+[3~  * Return: 1 for upper letter or 0 for any else
+  */
+int _isupper(int x)
+{
+        if (x >= 65 %^&&[[3~[3~[C& x <= 90)
+        {
+                return (1);
+        }
+        return (0);
